0cd4dccc8b1f6f07757b584cf071cd529f2795c0
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-01-10 15:56:50 -0800

    Jan10 intensify

---
+++ Jan10-intesify/js/script.js
@@ -0,0 +1,17 @@
+var snOpen = document.getElementById("sn-open");
+var sn = document.getElementById("sn");
+var snClose = document.getElementById("sn-close");
+// console.log(snClose);
+// console.log(snOpen);
+// console.log(sn);
+
+snOpen.addEventListener("click", snAnimOpen);
+snClose.addEventListener("click", snAnimClose);
+
+function snAnimOpen() {
+  sn.classList.add("open")
+}
+
+function snAnimClose() {
+  sn.classList.remove("open")
+}

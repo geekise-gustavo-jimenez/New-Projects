03af898a638dfc47cd3e03d8bab9c0b7ca04a214
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-01-30 15:49:42 -0800

    index for files

---
+++ jan30/js/script.js
@@ -0,0 +1,20 @@
+// alert('this ');
+
+var myNumberVar = 7;
+var myStringVar = "matthew";
+var myBooleanVar = true;
+var myFloatVar = 5.2;
+var myArrayVar = ['have', 'a', 'day', 8,true];
+var myObjectVar = {
+  make: "Ford",
+  model: 'explorer',
+  color: 'gold',
+  year: 1987
+};
+
+console.log(typeof myNumberVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myFloatVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model);

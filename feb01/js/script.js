b3eaaf66151e122abd2e3d78fd133d25eb6e60a6
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-01 15:53:46 -0800

    feb01 js

---
+++ feb01/js/script.js
@@ -0,0 +1,26 @@
+var carrots = 7;
+var brocoli = "gj";
+var callieflower = true;
+var lettuce = 8.9;
+var potato = ['have', 'a', 'day', 8,true];
+var radish = {
+  veggie: 'carrots',
+  fruit: 'tomato'
+};
+var pizza = 3;
+var better = pizza + carrots;
+var betterer = pizza / carrots;
+var bettererer = pizza * carrots;
+var betterererer = pizza - carrots;
+
+console.log(typeof carrots);
+console.log(typeof brocoli);
+console.log(typeof callieflower);
+console.log(typeof lettuce);
+console.log(typeof potato);
+console.log(typeof radish);
+
+console.log(better);
+console.log(betterer);
+console.log(bettererer);
+console.log(betterererer);

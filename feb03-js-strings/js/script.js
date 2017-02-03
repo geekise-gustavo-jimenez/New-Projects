fd38a2356db6ab985ad956a0bc69c05828c836ac
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-03 15:51:07 -0800

    index for files

---
+++ feb03-js-strings/js/script.js
@@ -0,0 +1,17 @@
+var firstString = 'a literal string';
+var secondString = new String("a string object");
+// var concatString = firstString.concat(secondString);
+var firstName = "Gustavo ";
+var lastName = "jimenez ";
+var born = 1999;
+var year = 2020;
+var answer =  year - born;
+
+var fullname = firstName + " Alexander Alfred " + lastName;
+
+console.log(fullname);
+
+var getIndex = firstString.indexOf('l', 8);
+
+console.log(getIndex);
+console.log(answer);
